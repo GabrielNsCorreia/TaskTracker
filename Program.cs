5a192775ed9c3e8790d6c2220a7b6aa42054a192
GabrielNsCorreia <correia.g@aluno.ifsp.edu.br>
2025-04-07 15:25:21 -0300

    Read
    Creating the CRUD manager with switch case and the "read" function

--- Program.cs
+++ Program.cs
@@ -1,12 +1,39 @@
-﻿namespace TaskTracker;
+﻿using System.ComponentModel;
+
+namespace TaskTracker;
 class Program
 {
     static void Main()
     {
-        Console.WriteLine("Hi");
-        List<TasksObjects> a = TasksManager.LoadTasks();
-        Console.WriteLine(a[0].Name);
-        a[0].Name = Console.ReadLine();
-        TasksManager.SaveTasks(a);
+        List<TasksObjects> tasks = TasksManager.LoadTasks();
+        
+        while (true)
+        {
+            Console.WriteLine("2 - Read Tasks \n5 - Exit");
+            Console.Write("Option: ");
+            string option = Console.ReadLine();
+            
+            switch (option)
+            {
+                case "2":
+                    Read(tasks);
+                    break;
+                case "5":
+                    return;
+                default:
+                    Console.WriteLine("Invalid option");
+                    break;
+            }
+            
+            TasksManager.SaveTasks(tasks);
+        }
+    }
+
+    static void Read(List<TasksObjects> tasks)
+    {
+        foreach (var task in tasks)
+        {
+            Console.WriteLine($"Name: {task.Name} \nAge: {task.Age}");
+        }
     }
 }
